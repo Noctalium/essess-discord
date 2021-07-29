ed6c3f013ca56cd70e900bdedc3eafc68f8821a8
Noctalium <noctaflygame@gmail.com>
2021-07-29 17:29:08 +0200

    changelog and help typo

---
+++ commands/other/changelog.js
@@ -0,0 +1,62 @@
+const Discord = require('discord.js');
+
+const { prefix } = require('../../config.json');
+const { getColorFromURL } = require('color-thief-node');
+
+module.exports = {
+    name: 'changelog',
+    description: `\`${prefix}changelog\` - Show Essess's changelog.`,
+    async execute(message, args) {
+        let color = await getColorFromURL('https://cdn.discordapp.com/attachments/383631230685544471/817324506019659776/diff-overdose.png');
+        let content = `
+        **v2.0.0 Changelog:**
+
+        **▸** Fixed bugs with the player command (\`${prefix}track (username)\`) where some tracks didn't ever worked
+
+        **▸** User with no profile picture can now use the bot
+
+        **▸** Fixed bug where, sometimes, randomly, commands didn't worked at all (blame peppy on this one)
+
+        **▸** Optimized the code, to prepare something else big (soontm) 👀
+
+        **->** To avoid useless spam, the \`${prefix}track (username)\` command do not allow players with less than **3000pp** to be tracked
+
+        ----------
+
+        v1.5.1 Changelog:
+
+        **+** Added the recent filter for the \`${prefix}top [username]\` command (ex: \`${prefix}top [username] -r\`)
+
+        `;
+
+        let embed = new Discord.MessageEmbed()
+        .setAuthor(
+            `Essess Changelog`,
+            `https://cdn.discordapp.com/attachments/383631230685544471/817324506019659776/diff-overdose.png`,
+            ``
+        )
+        .setDescription(content)
+        .setColor(color)
+        .setFooter(`Made by Noctalium#1621`);
+
+        return message.channel.send(embed);
+    }
+};
+
+/*
+**v1.4.0 Changelog:**
+
+        **+** Added the \`??top [username]\` command: Display the top 5 pp plays of user.
+
+        **+** Added the \`??mapfeed\` command: Track newly ranked beatmap and send a message every time a new one is ranked.
+
+        ----------
+
+**v1.5.0 Changelog:**
+
+        **+** Added the \`??changelog\` command: Display the most recent change to the bot.
+
+        **+** Added the \`??lbinfo\` command: Display information about the unranked leaderboard feature.
+
+        **->** Modified the \`??help\` command to make it better.
+*/
---
+++ commands/other/help.js
@@ -0,0 +1,38 @@
+const Discord = require('discord.js');
+
+const { prefix } = require('../../config.json');
+const { getColorFromURL } = require('color-thief-node');
+
+module.exports = {
+    name: 'help',
+    description: 'help',
+    async execute(message, args, commands) {
+
+        const hideCmd = ['mapfeed_msg', 'pp_tracking_msg', 'simulator', 'help',
+        'joke_bison_charge', 'joke_coolest', 'joke_finorza', 'joke_image_material',
+        'tracking_migration', 'ping'];
+
+        let embed = await generateDefaultEmbed(hideCmd, commands);
+        return message.channel.send(embed);
+    }
+}
+
+async function generateDefaultEmbed(hideCmd, commandsList) {
+    let description = ``;
+    commandsList.forEach(command => {
+        if(!hideCmd.includes(command.name)) {
+            description += command.description + `
+
+            `;
+        }
+    });
+    return new Discord.MessageEmbed()
+    .setAuthor(
+        `Command list:`,
+        `https://cdn.discordapp.com/attachments/383631230685544471/817324506019659776/diff-overdose.png`,
+        ``
+    )
+    .setDescription(description)
+    .setColor(await getColorFromURL('https://cdn.discordapp.com/attachments/383631230685544471/817324506019659776/diff-overdose.png'))
+    .setFooter(`{} -> Required | [] -> Optional -- If username is not given, command's author will be the default choice.`);
+}
