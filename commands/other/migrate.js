4879b9bb99726e8f8def83b699a72550d4a0d4ce
Noctalium <noctaflygame@gmail.com>
2021-07-29 16:32:06 +0200

    'typo'

--- commands/other/migrate.js
+++ commands/other/migrate.js
@@ -23,31 +23,33 @@ module.exports = {
 
             if(user.pp.raw < 3000) {
                 message.channel.send(`:x: ${user.name} do not have enough pp (${user.pp.raw})`);
-            }
+            } else {
+                let trackedUser = await sqlLib.getTrackedUser(user.id);
 
-            let trackedUser = await sqlLib.getTrackedUser(user.id);
-
-            if(trackedUser) {
+                if(trackedUser) {
 
                 // Check if user is already tracked in the given channel
                 let channelsArray = trackedUser.channels.split('|');
-    
+
                 if(!channelsArray.includes(oldUser.channel_id)) {
                     // Update after adding the channel id to the string
                     let newChannels = `${trackedUser.channels}${oldUser.channel_id}|`;
                     await sqlLib.updateTrackedUser(user, newChannels);
                     message.channel.send(`:white_check_mark: **${user.name}** is now tracked here.`);
-    
+
                 } else {
                     message.channel.send(`:x: This user is already tracked in the channel, use \`${prefix}untrack ${user.name}\` to remove it.`);
                 }
-    
-            } else {
-                // INSERT
-                let channels = oldUser.channel_id + '|';
-                await sqlLib.addTrackedUser(user, channels);
-                message.channel.send(`:white_check_mark: **${user.name}** is now tracked here.`);
+
+                } else {
+                    // INSERT
+                    let channels = oldUser.channel_id + '|';
+                    await sqlLib.addTrackedUser(user, channels);
+                    message.channel.send(`:white_check_mark: **${user.name}** is now tracked here.`);
+                }
             }
+
+            
        }
     }
 }
